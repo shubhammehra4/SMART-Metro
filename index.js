f48ada58d52935c225f6da3ad9541f9ef380c5f7
shubhammehra4 <shubhammehra4@gmail.com>
2021-01-27 13:49:01 +0530

    Routes Setup

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express"),
     bodyParser = require("body-parser"),
-    compression = require("compression"),
     dotenv = require("dotenv"),
     app = express();
 
@@ -12,11 +11,38 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//! Production
-// app.use(compression);
 
-app.get("/", (req, res) => {
+res.send("/", (req, res) => {
+    //TODO: Render a Landing Page
+    res.send("landing page go to /home");
+});
+
+app.get("/home", (req, res) => {
     res.render("home");
+});
+
+app.get("/map", (req, res) => {
+    //TODO: Render a Map
+    res.send("map");
+});
+
+app.get("/about", (req, res) => {
+    //TODO: Render About Us
+    res.send("about");
+});
+
+app.post("/findroute", async (req, res) => {
+    try {
+        const { start, end } = req.body;
+        // Spawn Python script
+        const res = { result: "The path" };
+        return res.status(200).json(res);
+    } catch (err) {
+        return res.status(500).json({
+            message: "Oops Something went wrong!",
+            err,
+        });
+    }
 });
 
 app.listen(PORT, function () {
