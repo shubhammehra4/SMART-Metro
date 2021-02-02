7c34f23ad6cfc6e4bc4f6770d5fabd28d1b01719
shubhammehra4 <shubhammehra4@gmail.com>
2021-02-03 01:13:03 +0530

    Home Page

--- public/scripts/submit.js
+++ public/scripts/submit.js
@@ -3,14 +3,22 @@ $(document).ready(function () {
         let start = $("#start").val();
         let end = $("#end").val();
         if (start && end) {
-            console.log(start);
             fetch(`/findpath?start=${start}&end=${end}`, {
                 method: "POST",
             })
                 .then((res) => res.json())
                 .then((res) => {
                     console.log(res);
-                    $("#respath").text("Machine is Learning right now!");
+                    let bfs = res.bfsRoute;
+                    bfs = bfs.split(",");
+                    let dijktra = res.dijktraRoute;
+                    dijktra = dijktra.split(",");
+                    let printBfs = bfs.map((n) => `<h1>${n}</h1>`);
+                    let printDijktra = dijktra.map((n) => `<h1>${n}</h1>`);
+
+                    $("#bfspath").html(printBfs);
+                    $("#dfspath").html(printDijktra);
+                    $("#sep").show();
                 })
                 .catch((err) => {
                     console.log(err);
